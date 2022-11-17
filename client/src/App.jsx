375cb8f99d27d597a7a495aee1407dcb9dc05e4c
David Amebley <davameb@gmail.com>
2022-11-17 03:54:18 +0200

    Add simple authentication restrictions to Navbar links

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Post from "./pages/Post";
 import Login from "./pages/Login";
-import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 
 
 const App = () => {
@@ -12,11 +12,11 @@ const App = () => {
   return (
     <BrowserRouter>
       <div>
-        <Navbar/>
+        <Navbar user={user}/>
           <Routes>
             <Route path='/' element={<Home/>} />
-            <Route path='/login' element={<Login/>} />
-            <Route path='/post/:id' element={<Post/>} />
+            <Route path='/login' element={user ? <Navigate to="/" /> : <Login/>} />   
+            <Route path='/post/:id' element={user ? <Post/> : <Navigate to="/login" />} />
           </Routes>
       </div>
     </BrowserRouter>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({user}) => {
   return (
     <div className='navbar' >
-        <span className="logo">Social Logins App</span>
+        <span className="logo">
+          <Link className='link' to="/">Social Logins App</Link>
+        </span>
+
+        {user ? (
         <ul className="list">
             <li className="listItem">
                 <img src="https://i.imgur.com/BOLWDJI.png" alt="user avatar" className="avatar" />
@@ -11,6 +16,9 @@ const Navbar = () => {
             <li className="listItem">David Amebley</li>
             <li className="listItem">Logout</li>
         </ul>
+        ) : 
+        (<Link className='link' to="login">Login</Link>)
+      }
     </div>
   )
 }
