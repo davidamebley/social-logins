d63f7978fb6932b8bfccb902b561ceb557de14a8
David Amebley <davameb@gmail.com>
2022-11-17 03:34:25 +0200

    Wrap Browser Router around Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,22 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Post from "./pages/Post";
 import Login from "./pages/Login";
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 
 
 const App = () => {
+  const user = false;
   return (
-    <div>
-      <Navbar/>
-      {/* <Home/> */}
-      <Login/>
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar/>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/login' element={<Login/>} />
+            <Route path='/post/:id' element={<Post/>} />
+          </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 
