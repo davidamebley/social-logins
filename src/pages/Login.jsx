89342810d8bfd7a642e297caf4210d5e7b991a5b
David Amebley <davameb@gmail.com>
2022-11-17 02:34:27 +0200

    Add Styling for Login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React from 'react';
+import Google from '../img/google.png'
+import Facebook from '../img/facebook.png'
+import Github from '../img/github.png'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='login'>
+        <h1 className="login-title">Choose a Login Method</h1>
+        <div className="wrapper">
+            <div className="left">
+                <div className="login-button google">
+                    <img src={Google} alt="" className="icon" />
+                    Google
+                </div>
+                <div className="login-button facebook">
+                    <img src={Facebook} alt="" className="icon" />
+                    Facebook
+                </div>
+                <div className="login-button github">
+                    <img src={Github} alt="" className="icon" />
+                    GitHub
+                </div>
+            </div>
+
+            <div className="center">
+                <div className="line" />
+                <div className="or">OR</div>
+            </div>
+
+            <div className="right">
+                <input type="text" placeholder='Username' />
+                <input type="text" placeholder='Password' />
+                <button className="submit">Login</button>
+            </div>
+        </div>
+    </div>
   )
 }
 
