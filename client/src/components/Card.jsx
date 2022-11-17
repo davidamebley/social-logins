4bc794c0ccbbcdf862e1145ab93fd8ddeb9b73af
David Amebley <davameb@gmail.com>
2022-11-17 04:08:52 +0200

    Display posts dynamically

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Card = ({post}) => {
   return (
     <div className='card'>
-      
+      <Link className='link' to={`/post/${post.id}`}>
         <span className="card-title">{post.title}</span>
         <img src={post.img} alt="" className="img" />
         <p className="desc"> {post.desc} </p>
         <button className="card-button">Read More</button>
+      </Link>
     </div>
   )
 }
--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 import { posts } from '../data'
 
 const Post = () => {
+const location = useLocation();
+const path = location.pathname.split('/')[2];   //path/post/id  'id' is index 2
 
-    const post = posts[2]
+const post = posts.find((p) => p.id.toString() === path);   // find the post whose id matches
+
+console.log(location)
   return (
     <div className='post'>
         <img src={post.img} alt="" className="post-img" />
